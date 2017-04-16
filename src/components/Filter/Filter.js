0a2fa1530444d11efce1d92bcaae5f12a8575cb3
Nikita Mashukov <mashukovnikita@gmail.com>
2017-04-16 15:45:17 +0300

    filtering

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,8 +2,90 @@ import React, {Component} from 'react';
 import './Filter.css';
 
 class Filter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeTransfers: []
+    };
+  }
+
+  componentDidMount() {
+    this.setActiveTransfersCount(this.props.transfers.slice());
+  }
+
   render() {
-    return <div>Filter</div>;
+    const props = this.props;
+    const state = this.state;
+
+    const activeTransfers = this.state.activeTransfers;
+    const countTransfers  = this.props.transfers
+      .sort(byCountAsc)
+      .map(count => (
+        <li
+          className={"filter-transfers-list__item" + (isCountActive(count) ? " filter-transfers-list__item--active" : "")}
+          onClick={this.toggleTransfersCount.bind(this, count)}
+          key={count}>
+          {count}
+        </li>
+      ));
+
+    return <div className="filter">
+      <div className="filter__title">КОЛИЧЕСТВО ПЕРЕСАДОК</div>
+
+      <div className="filter__transfers">
+        <ul className="filter-transfers-list">
+          <li className={"filter-transfers-list__item" + (isAllActive() ? " filter-transfers-list__item--active" : "")}
+              onClick={this.toggleAll.bind(this)}>Все
+          </li>
+          {countTransfers}
+        </ul>
+      </div>
+    </div>;
+
+    function byCountAsc(countA, countB) {
+      return countA < countB ? -1 : +1;
+    }
+
+    function isAllActive() {
+      return props.transfers.length === state.activeTransfers.length;
+    }
+
+    function isCountActive(count) {
+      return activeTransfers.indexOf(count) > -1;
+    }
+  }
+
+  toggleAll() {
+    const transfers        = this.props.transfers;
+    const activeTransfers  = this.state.activeTransfers;
+    let newActiveTransfers = null;
+
+    if (activeTransfers.length === transfers.length)
+      newActiveTransfers = [];
+    else
+      newActiveTransfers = transfers.slice();
+
+    this.setActiveTransfersCount(newActiveTransfers);
+  }
+
+  toggleTransfersCount(count) {
+    const activeTransfers    = this.state.activeTransfers;
+    const index              = activeTransfers.indexOf(count);
+    const newActiveTransfers = [...activeTransfers];
+
+    if (index > -1) newActiveTransfers.splice(index, 1);
+    else newActiveTransfers.push(count);
+
+    this.setActiveTransfersCount(newActiveTransfers)
+  }
+
+  setActiveTransfersCount(activeTransfers) {
+    this.setState({
+      activeTransfers: activeTransfers
+    });
+
+    this.props.onChange(activeTransfers);
   }
 }
 
--- src/components/TicketsPage/TicketsPage.js
+++ src/components/TicketsPage/TicketsPage.js
@@ -5,10 +5,24 @@ import './TicketsPage.css';
 import data from '../../../public/tickets.json';
 
 class TicketsPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filter: {
+        transfers: []
+      }
+    };
+  }
+
   render() {
     const tickets = data.tickets
+      .filter(ticket => this.state.filter.transfers.indexOf(ticket.stops) > -1)
       .sort(byPriceAsc)
       .map(ticketInfo => <Ticket data={ticketInfo} key={JSON.stringify(ticketInfo)}/>);
+
+    const transfers = data.tickets
+      .reduce(collectTransfers, []);
 
     return (
       <div className="tickets-page">
@@ -17,7 +31,7 @@ class TicketsPage extends Component {
         </div>
 
         <div className="tickets-page__instruments">
-          <Filter/>
+          <Filter transfers={transfers} onChange={this.handleFilterChange.bind(this)}/>
         </div>
 
         <div className="tickets-page__tickets">
@@ -29,6 +43,20 @@ class TicketsPage extends Component {
     function byPriceAsc(ticketA, ticketB) {
       return ticketA.price < ticketB.price ? -1 : +1;
     }
+
+    function collectTransfers(arr, ticket) {
+      if (arr.indexOf(ticket.stops) < 0) arr.push(ticket.stops);
+
+      return arr;
+    }
+  }
+
+  handleFilterChange(transfers) {
+    this.setState({
+      filter: {
+        transfers: transfers
+      }
+    });
   }
 }
 
